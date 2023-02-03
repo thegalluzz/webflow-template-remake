a63ff0e9edfda011cd6e9460910d175aa0ad4f4a
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-02-03 21:12:04 +0100

    added html structure for contract page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head"
 import styles from '@/styles/Home.module.css'
-import Navbar from "@/components/navbar"
+import Navbar from '@/components/navbar'
+import CallToAction from '@/components/cta'
+import Footer from '@/components/footer'
 
 export default function Contact() {
     return (
@@ -12,6 +14,33 @@ export default function Contact() {
             </Head>
             <main className={styles.main}>
                 <Navbar />
+                <section>
+                    <div>
+                        <div>
+                            <h1>Contact Us</h1>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <div>
+                        <div>
+
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+
+                </section>
+
+                <section>
+                    
+                </section>
+
+
+                <CallToAction />
+                <Footer />
             </main>
         </>
     )
