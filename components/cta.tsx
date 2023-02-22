097565d6dbeb92f7ebd9de4d55330b2fe331f910
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-02-23 00:31:50 +0100

    refactored css for cta section

--- components/cta.tsx
+++ components/cta.tsx
@@ -3,18 +3,20 @@ import Link from 'next/link'
 
 export default function CallToAction() {
     return (
-        <section>
-            <div className={styles.ctaWrapper}>
-                <div className={styles.introdiv}>
-                    <div className={styles.introtext}>
-                        <h2>Grow your business.</h2>
-                        <text>
-                            Today is the day to build the business of your dreams.
-                            Share your mission with the world — and blow your customers away.
-                        </text>
-                    </div>
+        <section className={styles.CtaSection}>
+            <div className={styles.CtaContainer}>
+                <div className={styles.ctaWrapper} >
                     <div>
-                        <Link href="/contact"><button className={styles.ctabutton}>START NOW</button></Link>
+                        <div className={styles.introtext}>
+                            <h2>Grow your business.</h2>
+                            <text>
+                                Today is the day to build the business of your dreams.
+                                Share your mission with the world — and blow your customers away.
+                            </text>
+                        </div>
+                        <div>
+                            <Link href="/contact"><button className={styles.ctabutton}>START NOW</button></Link>
+                        </div>
                     </div>
                 </div>
             </div>
