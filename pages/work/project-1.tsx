327c3f919c228ab67eb6a4b02aeef684bf924285
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-02-26 20:01:33 +0100

    minor fixes

--- pages/work/project-1.tsx
+++ pages/work/project-1.tsx
@@ -29,8 +29,9 @@ export default function ProjectOne() {
                             <div>
                                 <div>
                                     <p>DESCRIPTION</p>
-                                    <p  className={styles.ParagraphLight}>
-                                        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+                                    <p className={styles.ParagraphLight}>
+                                        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
+                                        there live the blind texts.
                                         Separated they live in Bookmarksgrove right at the coast.
                                     </p>
                                 </div>
@@ -46,18 +47,26 @@ export default function ProjectOne() {
                                 </div>
                             </div>
                             <p  className={styles.ParagraphLight}>
-                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
+                                there live the blind texts.
+                                Separated they live in Bookmarksgrove right at the coast of the Semantics, 
+                                a large language ocean.
+                                A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth. 
+                                <br /><br />
+                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
+                                there live the blind texts.
+                                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
+                                there live the blind texts.
                                 Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
                                 A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth. <br /><br />
-                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
-                                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
-                                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-                                A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth.<br /><br />
-                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
-                                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+                                <br /><br />
+                                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
+                                there live the blind texts.
+                                Separated they live in Bookmarksgrove right at the coast of the Semantics, 
+                                a large language ocean.
                             </p>
                         </div>
                     </div>
