7ffc0697319cf5fb909077b6021c12ad120a5d45
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-02-28 01:07:10 +0100

    moved navbar css to separate module

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,21 @@
 import Link from "next/link"
-import Image from "next/image"
-import styles from '@/styles/Home.module.css'
-
+import styles from './navbar.module.css'
 
 export default function Navbar() {
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navbarwrapper}>
+        <nav className={styles.Navbar}>
+            <div className={styles.NavbarWrapper}>
                 <Link href='/'><img src="/business-logo.png" alt="logo" className={styles.navlogo}/></Link>
-                <div className={styles.menu}>
-                    <div className={styles.navigationitems}>
-                        <Link href='/about' className={styles.navigationitem}>ABOUT</Link>
-                        <Link href='/projects' className={styles.navigationitem}>WORK</Link>
-                        <Link href='/team' className={styles.navigationitem}>TEAM</Link>
-                        <Link href='/blog' className={styles.navigationitem}>BLOG</Link>
-                        <Link href='/contact' className={styles.navigationitem}>CONTACT</Link>
+                <div className={styles.Menu}>
+                    <div className={styles.NavigationItems}>
+                        <Link href='/about' className={styles.NavigationItem}>ABOUT</Link>
+                        <Link href='/projects' className={styles.NavigationItem}>WORK</Link>
+                        <Link href='/team' className={styles.NavigationItem}>TEAM</Link>
+                        <Link href='/blog' className={styles.NavigationItem}>BLOG</Link>
+                        <Link href='/contact' className={styles.NavigationItem}>CONTACT</Link>
                     </div>
                 </div>
-                <Link href='mailto:example@example.com'><button className={styles.mainbutton} >CONTACT US</button></Link>
+                <Link href='mailto:example@example.com'><button className={styles.MainButton} > CONTACT US </button></Link>
             </div>
         </nav>
     )
