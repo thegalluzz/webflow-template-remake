2ff658ea7eacca9c2f7a4480d0d6a7656c06ca45
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-03-02 23:57:50 +0100

    minor fix navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,7 +24,6 @@ export default function Navbar() {
                     </div>
                 </div>
             </nav>
-
             {
                 (navigationItems)
                     ?
