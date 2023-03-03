ee8f0274b4b7a38624225aff99cb116551dc02cb
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-03-04 00:44:24 +0100

    updated css of components tobe mobile responsive

--- components/call-to-action/call-to-action.tsx
+++ components/call-to-action/call-to-action.tsx
@@ -5,17 +5,23 @@ export default function CallToAction() {
     return (
         <section className={styles.CtaSection}>
             <div className={styles.CtaContainer}>
-                <div className={styles.CtaWrapper} >
+                <div className={styles.CtaWrapper}>
                     <div>
                         <div className={styles.IntroText}>
-                            <h2 className="HeadingJumboSmall"> Grow your business. </h2>
+                            <h2 className="HeadingJumboSmall">
+                                Grow your business.
+                            </h2>
                             <p className="ParagraphBiggerBlack">
                                 Today is the day to build the business of your dreams.
                                 Share your mission with the world — and blow your customers away.
                             </p>
                         </div>
                         <div>
-                            <Link href="/contact"><button className={styles.CtaButton}> START NOW </button></Link>
+                            <Link href="/contact">
+                                <button className={styles.CtaButton}>
+                                    START NOW
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
