de023379aec7020b43c7077d58b3ebb4017c736a
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-03-16 00:38:14 +0100

    added self-hosted font

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
-                <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' />
+                {/*<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' />*/}
             </Head>
             <main className="Main">
                 <Navbar />
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
-                <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' />
+                {/*<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' />*/}
             </Head>
             <main className="Main">
                 <Navbar />
