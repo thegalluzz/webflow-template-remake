1177b61667b61d04c66480dde26c188896d5a27b
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-04-17 20:57:29 +0200

    minor linting fix

--- components/for-o-for/for-o-for.tsx
+++ components/for-o-for/for-o-for.tsx
@@ -4,7 +4,7 @@ export default function ForOFor(){
     return(
         <div className={styles.ForOForDiv}>
         <h1>Page Not Found</h1>
-        <p>The page you are looking for doesn't exist or has been moved.</p>
+        <p>The page you are looking for doesn&apos;t exist or has been moved.</p>
         <a href='/'>
           <button className={styles.ForOForButton}>GO BACK TO SAFETY</button>
         </a>
