844ba83a17740e5386c55f5a7f565726ea66b1f7
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-04-17 21:41:37 +0200

    removed obsolete google fonts link from header

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,6 @@ export default function FourZeroFour() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        {/*<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' />*/}
       </Head>
       <ForOFor />
     </>
