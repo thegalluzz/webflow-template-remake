0bd4e42f47c77efa0f589ddea0795ed278d6d5ee
thegalluzz <gabrielet.galluzzo@gmail.com>
2023-08-31 10:17:29 +0200

    added head in privacy policy

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,10 +1,17 @@
 
 import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
 
 
 export default function Privacy() {
     return (
         <>
+            <Head>
+                <title>Privacy Policy</title>
+                <meta name="description" content="Read our policy" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <section className={styles.PrivacyPolicySection}>
                 <div>
                     <h1>Privacy Policy</h1>
